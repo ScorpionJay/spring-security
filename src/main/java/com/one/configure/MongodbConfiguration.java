9d0a04a3a1ddfad713907f588fa365b3e59dec1f
jay <jay.wang.scorpion@gmail.com>
2016-07-07 17:00:07 +0800

    mongodb auth

--- src/main/java/com/one/configure/MongodbConfiguration.java
+++ src/main/java/com/one/configure/MongodbConfiguration.java
@@ -1,5 +1,8 @@
 package com.one.configure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 /**
  * @author jay
@@ -25,6 +30,16 @@ public class MongodbConfiguration extends AbstractMongoConfiguration {
 
 	@Value("${mongo.db}")
 	private String DB;
+	
+	@Value("${mongo.username}")
+	private String username;
+	
+	@Value("${mongo.password}")
+	private String password;
+	
+	@Value("${mongo.auth}")
+	private Boolean auth;
+	
 
 	@Override
 	protected String getDatabaseName() {
@@ -34,6 +49,21 @@ public class MongodbConfiguration extends AbstractMongoConfiguration {
 	@Bean
 	@Override
 	public Mongo mongo() throws Exception {
-		return new MongoClient(MONGO_DB_HOST, MONGO_DB_PORT);
+		if(auth){
+			ServerAddress serverAddress = new ServerAddress(MONGO_DB_HOST,MONGO_DB_PORT);  
+	         List<ServerAddress> addrs = new ArrayList<ServerAddress>();  
+	         addrs.add(serverAddress);  
+	           
+	         //MongoCredential.createScramSha1Credential()三个参数分别为 用户名 数据库名称 密码  
+	         MongoCredential credential = MongoCredential.createScramSha1Credential(username, "admin", password.toCharArray());  
+	         List<MongoCredential> credentials = new ArrayList<MongoCredential>();  
+	         credentials.add(credential);  
+	           
+	         //通过连接认证获取MongoDB连接  
+	         MongoClient mongoClient = new MongoClient(addrs,credentials);  
+	         return mongoClient;
+		}else{
+			return new MongoClient(MONGO_DB_HOST, MONGO_DB_PORT);
+		}
 	}
 }
